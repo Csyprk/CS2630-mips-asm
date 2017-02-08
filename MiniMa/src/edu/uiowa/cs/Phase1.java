a1e8ec72b0acd152ba9f9c093b96b2709b32e751
ryan <ryan-polley@uiowa.edu>
2017-02-08 11:54:01 -0600

    implemented branch psuedoinstructions

--- MiniMa/src/edu/uiowa/cs/Phase1.java
+++ MiniMa/src/edu/uiowa/cs/Phase1.java
@@ -13,6 +13,41 @@ public class Phase1 {
      * returns a list of TAL instructions (should be same size or longer than input list)
      */
     public static List<Instruction> mal_to_tal(List<Instruction> mals) {
+	List<Instruction> tals = new LinkedList<Instruction>();
+	for(Instruction instr : mals){
+		//determine if instruction is in tal
+		int id = instr.instruction_id;
+		switch (instr.instruction_id){
+			case 100://blt r1, r2, label
+			//slt $1, r1, r2 
+			Instruction slt1 = new Instruction(9,1,instr.rs,instr.rt,
+							 0,0,0,instr.label_id,
+							 instr.branch_label);
+			tals.add(slt1);
+			//beq $1, $0, label
+			Instruction beq1 = new Instruction(5,0,1,0,0,0,0,
+							  instr.label_id,
+							  instr.branch_label);
+			tals.add(beq1);
+			break;
+			case 101: //bge r1, r2, label
+			//slt $1, r1, r2
+			Instruction slt2 = new Instruction(9,1,instr.rs,instr.rt,
+							 0,0,0,instr.label_id,
+							 instr.branch_label);
+			tals.add(slt2);
+			//bne $1, $0, label
+			Instruction bne2 = new Instruction(6,0,1,0,0,0,0,
+							  instr.label_id,
+							  instr.branch_label);
+			tals.add(bne2);
+			break;
+			default:
+			//instruction in tal
+			tals.add(instr);
+		}
+		
+	}
         return null;
     }
 }
