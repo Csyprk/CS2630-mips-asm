b01e727be77e3bd77195c3bb304cedd8c7fd9d1a
hekemp <heather-kemp@uiowa.edu>
2017-02-12 19:54:17 -0600

    Completed preliminary phase 2 functionality

--- MiniMa/src/edu/uiowa/cs/Phase2.java
+++ MiniMa/src/edu/uiowa/cs/Phase2.java
@@ -16,8 +16,42 @@ public class Phase2 {
      * unresolved: list of instructions without resolved addresses
      * first_pc: address where the first instruction will eventually be placed in memory
      */
+    
+    // Branch instruction codes: 
+    //5             beq
+    //6               bne
+    
+    //int label_id;      // 0=no label on this line; nonzero is a unique id (what we need to look at)
+    //int branch_label;  // label used by branch or jump instructions (what we need to look for)
+
     public static List<Instruction> resolve_addresses(List<Instruction> unresolved, int first_pc) {
-        return null;
+        List<Instruction> resolved = new LinkedList<Instruction>(unresolved);
+        for(int i = 0; i < resolved.size(); i ++)
+        {// This is a branch instruction, so make the appropriate changes, which are universal per event
+            if(resolved.get(i).instruction_id == 5 || resolved.get(i).instruction_id == 6)
+         {
+          int branchLabel = resolved.get(i).branch_label;
+          int j = 0; 
+          for(int k = 0; k < resolved.size(); k++)
+          {if(resolved.get(i).label_id == branchLabel)
+              {j = k;
+               k = resolved.size() + 1;
+              }
+            }
+          int nextDistance = 4 * j;
+          int nextAddress = 0;
+          if (i < j)
+            nextAddress = (i - j) + 1 ;
+          else
+            nextAddress = (j - i) - 1;
+            
+            resolved.get(i).immediate = nextAddress;
+            }
+            
+         // This is not a branch instruction, so leave it alone
+         else;
+        }
+        return resolved;
     }
 
 }
